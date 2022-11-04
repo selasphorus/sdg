cc8b9c335c37f322e4d0a50740064da553cb143c
atc <birdhive@gmail.com>
2022-11-04 10:52:54 -0400

    cont.

--- sdg.php
+++ sdg.php
@@ -227,7 +227,7 @@ $admin_functions_filepath = $plugin_path . 'inc/'.'admin_functions.php';
 $common_functions_filepath = $plugin_path . 'inc/'.'common_functions.php';
 $modules = get_option( 'modules', array( 'events', 'people', 'lectionary', 'music', 'webcasts', 'sermons', 'slider', 'ninjaforms' ) );
 
-//if ( file_exists($admin_functions_filepath) ) { include_once( $admin_functions_filepath ); } else { echo "no $admin_functions_filepath found"; }
+if ( file_exists($admin_functions_filepath) ) { include_once( $admin_functions_filepath ); } else { echo "no $admin_functions_filepath found"; }
 if ( file_exists($common_functions_filepath) ) { include_once( $common_functions_filepath ); } else { echo "no $common_functions_filepath found"; }
 
 foreach ( $includes as $inc ) {
