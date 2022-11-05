918e56c5391672a3b2a00237c05766b3dd5f7a8d
atc <birdhive@gmail.com>
2022-11-04 20:56:01 -0400

    cont.

--- sdg.php
+++ sdg.php
@@ -370,7 +370,7 @@ foreach ( $includes as $inc ) {
 
 foreach ( $modules as $module ) {
     $filepath = $plugin_path . 'modules/'.$module.'.php';
-    $arr_exclusions = array ( 'admin_notes', 'data_tables', 'ensembles', 'organizations', 'projects' ); // lectionary
+    $arr_exclusions = array ( 'admin_notes', 'data_tables', 'ensembles', 'organizations', 'projects', 'press' ); // lectionary
     if ( !in_array( $module, $arr_exclusions) ) { // skip modules w/ no files
     	if ( file_exists($filepath) ) { include_once( $filepath ); } else { echo "no $filepath found"; }
     }
