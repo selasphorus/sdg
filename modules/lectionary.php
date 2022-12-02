27ac3d9f8a7165b2e119eae29635b19135171a06
atc <birdhive@gmail.com>
2022-12-01 19:16:14 -0500

    cont.

--- modules/lectionary.php
+++ modules/lectionary.php
@@ -459,7 +459,7 @@ function calc_litdates( $atts = [] ) {
             )
         );
         $liturgical_date_calc_post = new WP_Query( $args );
-        $calc_info .= "liturgical_date_calc_post: ".print_r( $liturgical_date_calc_post, true )."<br />";
+        $calc_info .= "liturgical_date_calc_post: <pre>".print_r( $liturgical_date_calc_post, true )."</pre><br />";
         $liturgical_date_calc_id = $liturgical_date_calc_post->ID;
         $calc_info .= $indent."liturgical_date_calc_id: $liturgical_date_calc_id<br />";
         
