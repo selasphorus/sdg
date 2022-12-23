f2457bd27000035f796563946c8111193f5209a4
atc <birdhive@gmail.com>
2022-12-23 18:49:21 -0500

    cont.

--- modules/events.php
+++ modules/events.php
@@ -2129,7 +2129,7 @@ function get_special_date_content( $the_date = null ) {
         
         $fixed_date_str = date("F d", $timestamp ); // day w/ leading zeros
         $info .= '<div class="message centered">';
-        $info .= '<p class="narrow">Saint Thomas Church will be open to the public during the following hours on Christmas Eve:<br />';
+        $info .= '<p class="scalloped narrow">Saint Thomas Church will be open to the public during the following hours on Christmas Eve:<br />';
         $info .= "10am-1pm<br />3pm-6:30pm<br />9:30pm-12:30am</p>";
         $info .= '</div>';
     }
