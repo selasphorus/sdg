2c7cbfe5ce65398e70851e20964960fded33a8f0
atc <birdhive@gmail.com>
2023-01-11 10:19:38 -0500

    cont.

--- inc/common_functions.php
+++ inc/common_functions.php
@@ -387,8 +387,8 @@ function sdg_merge_form ($atts = [], $content = null, $tag = '') {
 		}
 			
 		$info .= '<table>';
-		$info .= '<tr><th colspan="2">1</th><th colspan="2">2</th></tr>';
-		$info .= '<tr><th width="100px">Field Type</th><th width="180px">Field Name</th><th>P1 Value</th><th>Merged</th><th>P2 Value</th></tr>';
+		$info .= '<tr><th colspan="3">1</th><th colspan="3">2</th></tr>';
+		$info .= '<tr><th></th><th width="100px">Field Type</th><th width="180px">Field Name</th><th>P1 Value</th><th>Merged</th><th>P2 Value</th></tr>';
 		
 		foreach ( $arr_fields as $field_name => $values ) {
 		
