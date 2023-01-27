9002f44033a73d96e39e0b179aafebc2cc8ddecb
atc <birdhive@gmail.com>
2023-01-27 09:46:43 -0500

    Pass RO data in original form, not imploded

--- inc/common_functions.php
+++ inc/common_functions.php
@@ -490,7 +490,7 @@ function sdg_merge_form ($atts = [], $content = null, $tag = '') {
 				} else {
 					$info .= '<td>';
 					$info .= 'field_type: '.$field_type.'<br /><span class="nb">'.$merge_value_str.'</span>'.$merge_info;
-					$info .= '<input type="hidden" name="'.$field_name.'" value="'.$merge_value_str.'" />';
+					$info .= '<input type="hidden" name="'.$field_name.'" value="'.$merge_value.'" />';
 					$info .= '</td>';
 				}				
 				//$info .= '<td><textarea name="'.$field_name.'" rows="5" columns="20">'.$merge_value_str.'</textarea>'.$merge_info.'</td>';
