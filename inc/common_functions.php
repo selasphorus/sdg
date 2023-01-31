4e9918c735f2ccaae2c26277cca45756145b1ea8
atc <birdhive@gmail.com>
2023-01-31 14:07:41 -0500

    cont.

--- inc/common_functions.php
+++ inc/common_functions.php
@@ -492,19 +492,19 @@ function sdg_merge_form ($atts = [], $content = null, $tag = '') {
 				
 					// Do the merging...
 					$old_val = $p1_val;
-					if ( is_array($old_val) ) { $old_val_str = print_r($old_val, true); } else  { $old_val_str = $old_val; }
+					if ( is_array($old_val) ) { $old_val_str = print_r($old_val, true); } else { $old_val_str = $old_val; }
 					
 					$new_val = "";
 					if ( !empty($_POST[$field_name]) ) {
 						$new_val = trim($_POST[$field_name]);
 					}
-					if ( is_array($new_val) ) { $new_val_str = print_r($new_val, true); } else  { $new_val_str = $new_val; }
+					if ( is_array($new_val) ) { $new_val_str = print_r($new_val, true); } else { $new_val_str = $new_val; }
 					
 					if ( !empty($old_val) || !empty($new_val) ) {
 						if ( $old_val_str != $new_val_str ) {
 							$info .= "[$field_name] old_val_str: '$old_val_str';<br />[$field_name] new_val_str: '$new_val_str'<br />";
 							// update value
-							$info .= "New value not same as old for -> run update<br />";
+							$info .= "New value not same as old for $field_name -> run update<br />";
 						} else {
 							//$info .= "New value same as old for $field_name<br />";
 						}
