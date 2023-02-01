dc4139a1067648f708fed8bbb7ee7fa53259c5f5
atc <birdhive@gmail.com>
2023-02-01 17:23:37 -0500

    cont.

--- inc/common_functions.php
+++ inc/common_functions.php
@@ -301,10 +301,10 @@ function sdg_merge_form ($atts = [], $content = null, $tag = '') {
 				//$troubleshooting .= "arr_posts: <pre>".print_r($arr_posts,true)."</pre>"; // tft
 			
 				if ( count($arr_posts) < 2 ) {
-					$troubleshooting .= "<p>That's not enough posts! I need two posts, please.</p>";
+					$info .= "<p>That's not enough posts! I need two posts, please.</p>";
 					$troubleshooting .= "arr_posts: <pre>".print_r($arr_posts,true)."</pre>"; // tft
 				} else if ( count($arr_posts) > 2 ) {
-					$troubleshooting .= "<p>That's too many posts! I can only handle two at a time.</p>";
+					$info .= "<p>That's too many posts! I can only handle two at a time.</p>";
 					$troubleshooting .= "arr_posts: <pre>".print_r($arr_posts,true)."</pre>"; // tft
 				}
 		
