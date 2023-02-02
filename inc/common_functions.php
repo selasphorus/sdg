47d7224f4a1ef92740d91c5f499de1789f3b91b9
atc <birdhive@gmail.com>
2023-02-02 12:36:06 -0500

    Clarified comments re quotation marks issue

--- inc/common_functions.php
+++ inc/common_functions.php
@@ -615,7 +615,7 @@ function sdg_merge_form ($atts = [], $content = null, $tag = '') {
 							
 						} else if ( $field_cat == "acf_field" ) {
 						
-							//$new_value = esc_attr($new_val); // without this, the update fails for records with quotation marks, but WITH, it saves the backslashes. What to do? TODO: figure this out...
+							//$new_value = esc_attr($new_val); // without this, the update fails for records with *single* quotation marks, but WITH, it saves the backslashes. What to do? TODO: figure this out...
 							//$new_val = wp_slash($new_val);
 							
 							// convert new_val to array, if needed -- check field type >> explode
