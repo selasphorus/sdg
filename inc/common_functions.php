4e3d9212ba0332613b997a1345652aea8fd9c322
atc <birdhive@gmail.com>
2023-02-21 16:31:34 -0500

    cont.

--- inc/common_functions.php
+++ inc/common_functions.php
@@ -416,9 +416,9 @@ function sdg_merge_form ($atts = [], $content = null, $tag = '') {
 			//$info .= 'p2: <pre>'.print_r($p2,true).'</pre>'; // tft
 			//
 			$info .= "<pre>";
-			$info .= "Post #1 [ ID: ".$p1_id. "] >> Last modified: ".$p1->post_modified."; author: ".get_the_author_meta('user_nicename',$p1->post_author);
+			$info .= "Post #1 [ID: ".$p1_id. "] >> Last modified: ".$p1->post_modified."; author: ".get_the_author_meta('user_nicename',$p1->post_author);
 			$info .= '&nbsp;<a href="'.get_permalink($p1_id).'" target="_blank">View</a>&nbsp;|&nbsp;<a href="'.get_edit_post_link($p1_id).'" target="_blank">Edit</a><br />';
-			$info .= "Post #2 [ ID: ".$p2_id. "] >> Last modified: ".$p2->post_modified."; author: ".get_the_author_meta('user_nicename',$p2->post_author);
+			$info .= "Post #2 [ID: ".$p2_id. "] >> Last modified: ".$p2->post_modified."; author: ".get_the_author_meta('user_nicename',$p2->post_author);
 			$info .= '&nbsp;<a href="'.get_permalink($p2_id).'" target="_blank">View</a>&nbsp;|&nbsp;<a href="'.get_edit_post_link($p2_id).'" target="_blank">Edit</a><br />';
 			$info .= "</pre>";
 			//
