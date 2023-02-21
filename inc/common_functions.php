a960271f88b7edd21c3562c5b26567372946a6ed
atc <birdhive@gmail.com>
2023-02-21 11:25:43 -0500

    display fine-tuning

--- inc/common_functions.php
+++ inc/common_functions.php
@@ -790,8 +790,8 @@ function sdg_merge_form ($atts = [], $content = null, $tag = '') {
 					$info .= '<span class="nb">'."ERROR! failed to move p2 [".$_POST['p2_id']."] to trash.</span><br />";
 				}
 				
-				$info .= 'Post #1 >>&nbsp;<a href="'.get_permalink($p1_id).'" target="_blank">View</a>&nbsp;<a href="'.get_edit_post_link($p1_id).'" target="_blank">Edit</a><br />';
-				$info .= 'Post #2 >>&nbsp;<a href="'.get_permalink($p2_id).'" target="_blank">View</a>&nbsp;<a href="'.get_edit_post_link($p2_id).'" target="_blank">Edit</a><br />';
+				$info .= 'Post #1 >>&nbsp;<a href="'.get_permalink($p1_id).'" target="_blank">View</a>&nbsp;|&nbsp;<a href="'.get_edit_post_link($p1_id).'" target="_blank">Edit</a><br />';
+				$info .= 'Post #2 >>&nbsp;<a href="'.get_permalink($p2_id).'" target="_blank">View</a>&nbsp;|&nbsp;<a href="'.get_edit_post_link($p2_id).'" target="_blank">Edit</a><br />';
 				
 			} else {
 				$info .= "<h3>Errors occurred during Merge operation. Therefore p2 [".$_POST['p2_id']."] has not yet been moved to the trash.</h3>";
