5e9a0eb68988b2ebb2a88a3cd60f9228729be379
atc <birdhive@gmail.com>
2023-02-21 15:47:51 -0500

    cont.

--- modules/music.php
+++ modules/music.php
@@ -1056,8 +1056,8 @@ function sdg_search_form ($atts = [], $content = null, $tag = '') {
 		'post_type'       => array( $post_type ), // Single item array, for now. May add other related_post_types -- e.g. repertoire; edition
 		'post_status'     => 'publish',
 		'posts_per_page'  => $limit, //-1, //$posts_per_page,
-        'orderby'         => 'title ID',
-        'order'           => 'ASC',
+        'orderby'         => array( 'title' => 'ASC', 'ID' => 'ASC' ), //'title',
+        //'order'           => 'ASC',
         'return_fields'   => 'ids',
 	);
     
