94b664a76240d576f841226867d1d127ebd7b695
atc <birdhive@gmail.com>
2023-05-01 20:14:20 -0400

    cont.

--- modules/events.php
+++ modules/events.php
@@ -1707,22 +1707,22 @@ function sdg_placeholders( $replace, $EM_Event, $result ) {
     
     if ( $result == '#_EVENTLINK' ) { $make_link = true; } else { $make_link = false; }
     
-    // Get the formatted event title -- WIP!
+    // Get the formatted event title
 	$title_args = array( 'post' => $post_id, 'link' => $make_link, 'line_breaks' => false, 'show_subtitle' => true, 'echo' => false, 'hlevel' => 0, 'hlevel_sub' => 0 );
     $ts_info .= "title_args: ".print_r($title_args,true);
     $event_title = stc_post_title( $title_args );
-    //$event_title = "TEST"; // tft
     
     if ( $result == '#_EVENTNAME' ) {
     
-    	$replace .= " [_EVENTNAME] >> ".$event_title." << ";
-    	$replace .= $ts_info;
+    	$replace = $event_title;
+    	//$replace .= " [_EVENTNAME] >> ".$event_title." << ";
+    	//$replace .= $ts_info;
     
     } else if ( $result == '#_EVENTLINK' ) {
         
-        //$replace = $event_title;
-        $replace .= " [_EVENTLINK] >> ".$event_title." << ";
-        $replace .= $ts_info;
+        $replace = $event_title;
+        //$replace .= " [_EVENTLINK] >> ".$event_title." << ";
+        //$replace .= $ts_info;
         
     } else if ( $result == '#_EDITEVENTLINK' ) {
         
