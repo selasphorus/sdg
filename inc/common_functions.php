f89e971f35e06845f48139e1c95804795a79c96e
atc <birdhive@gmail.com>
2023-05-05 14:04:32 -0400

    cont.

--- inc/common_functions.php
+++ inc/common_functions.php
@@ -408,7 +408,7 @@ function sdg_post_thumbnail ( $post_id = null, $img_size = "thumbnail", $use_cus
     	$info .= $img_html;
     } else { // $return == "id"
     	$info = $img_id;
-    	//$info .= '<div class="troubleshooting">'.$ts_info.'</div>';
+    	$info .= '<div class="troubleshooting">'.$ts_info.'</div>';
     }
 	
 	if ( $echo == true ) {
