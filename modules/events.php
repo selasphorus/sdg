dc18a793e3a4fb890750ed136044130afe21a0fc
atc <birdhive@gmail.com>
2023-05-08 13:57:41 -0400

    cont.

--- modules/events.php
+++ modules/events.php
@@ -1753,11 +1753,13 @@ function sdg_placeholders( $replace, $EM_Event, $result ) {
         $featured_image_display = get_field('featured_image_display', $post_id);
         $ts_info .= "<!-- featured_image_display: ".$featured_image_display." -->";
         
-        if ( !is_archive() ) { //&& ! ( is_page() && $post_id == get_the_ID() )
+        if ( !is_archive() && !is_page() ) { //&& ! ( is_page() && $post_id == get_the_ID() )
         
         	if ( $featured_image_display == "thumbnail" ) {
+        	
         		$show_image = false;
         		$replace = "<!-- featured_image_display: $featured_image_display -->";
+        		
         	} else if ( has_term( 'webcasts', 'event-categories', $post_id ) 
 				|| has_term( 'webcasts', 'category', $post_id ) 
 				|| has_term( 'video-webcasts', 'event-categories', $post_id ) 
