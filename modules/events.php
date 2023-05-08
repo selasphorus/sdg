2ac64283775a639c5eb49b90e78d959cde4aaa8f
atc <birdhive@gmail.com>
2023-05-08 13:54:06 -0400

    cont.

--- modules/events.php
+++ modules/events.php
@@ -1753,7 +1753,7 @@ function sdg_placeholders( $replace, $EM_Event, $result ) {
         $featured_image_display = get_field('featured_image_display', $post_id);
         $ts_info .= "<!-- featured_image_display: ".$featured_image_display." -->";
         
-        if ( !is_archive() && !is_page(204597) ) {
+        if ( !is_archive() && ! ( is_page() && $post_id == get_the_ID() ) ) {
         
         	if ( $featured_image_display == "thumbnail" ) {
         		$show_image = false;
