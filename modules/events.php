d76936747db8c001b8ddf31f0d9f2c3ac9948598
atc <birdhive@gmail.com>
2023-06-21 18:36:19 -0400

    cont.

--- modules/events.php
+++ modules/events.php
@@ -1436,8 +1436,8 @@ function get_program_item_name ( $args = array() ) {
 
 			}
 			
-			$ts_info .= "<!-- title_as_label: '$title_as_label' -->";
-			$ts_info .= "<!-- item_name: '$item_name' -->";
+			//$ts_info .= "<!-- title_as_label: '$title_as_label' -->";
+			//$ts_info .= "<!-- item_name: '$item_name' -->";
 			
 			$program_title_as_label .= $title_as_label;
 			$program_item_name .= $item_name;
