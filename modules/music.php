4fa251bc37d8ccc14a0897f73e6585b24cc0bd0a
atc <birdhive@gmail.com>
2023-06-21 18:33:45 -0400

    cont.

--- modules/music.php
+++ modules/music.php
@@ -802,7 +802,7 @@ function get_authorship_info ( $args = array() ) {
             } else if ( $format != 'edition_title' && $format != "concert_item" ) {
                 //$authorship_info .= " -- ";
             }
-            if ( $authorship_info != "" ) { $authorship_info .= ", "; } else { $authorship_info .= " -- "; }
+            if ( $authorship_info != "" ) { $authorship_info .= ", "; } else if ( $format != "concert_item" ) { $authorship_info .= " -- "; }
             if ( $html ) { 
             	$authorship_info .= '<span class="arranger">arr. '.$arrangers_info.'</span>';
             } else {
