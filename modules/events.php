e15a1e30aeb746b521f96eec306d4f2fc72fe6c7
atc <birdhive@gmail.com>
2023-06-26 12:36:26 -0400

    fixed var names

--- modules/events.php
+++ modules/events.php
@@ -1674,11 +1674,11 @@ function event_program_row_cleanup ( $post_id = null, $repeater_name = null, $i 
 		}
 		
 		// Clear out empty fields not needed per row_type
-		if ( row_type == "label_only" ) {
+		if ( $row_type == "label_only" ) {
 			// If row_type == "label_only" and program_item/program_item_txt is/are empty, delete the empty meta rows
 			if ( empty($program_item) && metadata_exists( 'post', $post_id, $repeater_name.'_'.$i.'_program_item' ) ) { $arr_field_deletions[] = "program_item"; }
 			if ( empty($program_item_txt) && metadata_exists( 'post', $post_id, $repeater_name.'_'.$i.'_program_item_txt' ) ) { $arr_field_deletions[] = "program_item_txt"; }
-		} else if ( row_type == "title_only" ) {
+		} else if ( $row_type == "title_only" ) {
 			// If row_type == "title_only" and item_label/item_label_txt is/are empty, delete the empty meta rows
 			if ( empty($item_label) && metadata_exists( 'post', $post_id, $repeater_name.'_'.$i.'_item_label' ) ) { $arr_field_deletions[] = "item_label"; }
 			if ( empty($item_label_txt) && metadata_exists( 'post', $post_id, $repeater_name.'_'.$i.'_item_label_txt' ) ) { $arr_field_deletions[] = "item_label_txt"; }
