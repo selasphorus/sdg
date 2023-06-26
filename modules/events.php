9744d705598476b353fd806f59c2114fd380c54e
atc <birdhive@gmail.com>
2023-06-26 13:02:34 -0400

    cont.

--- modules/events.php
+++ modules/events.php
@@ -1677,7 +1677,8 @@ function event_program_row_cleanup ( $post_id = null, $repeater_name = null, $i 
 		if ( $row_type == "label_only" || $row_type == "program_note" ) {
 			// If row_type == "label_only" and program_item/program_item_txt is/are empty, delete the empty meta rows
 			if ( empty($program_item) && metadata_exists( 'post', $post_id, $repeater_name.'_'.$i.'_program_item' ) ) { $arr_field_deletions[] = "program_item"; }
-		} else if ( $row_type == "title_only" || $row_type == "program_note" ) {
+		}
+		if ( $row_type == "title_only" || $row_type == "program_note" ) {
 			// If row_type == "title_only" and item_label/item_label_txt is/are empty, delete the empty meta rows
 			if ( empty($item_label) && metadata_exists( 'post', $post_id, $repeater_name.'_'.$i.'_item_label' ) ) { $arr_field_deletions[] = "item_label"; }
 		}
