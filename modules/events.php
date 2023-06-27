d44b90b447f0b33f09e254a2fd4237c04fbdfb6c
atc <birdhive@gmail.com>
2023-06-26 22:52:22 -0400

    cont.

--- modules/events.php
+++ modules/events.php
@@ -2496,7 +2496,7 @@ function event_program_cleanup( $atts = [] ) {
         
         	$info .= "<h2>Personnel</h2>";
 			//$info .= "Found ".count($posts)." event post(s) with program postmeta.<br /><br />"; //$info .= "Found ".count($posts)." event post(s) with personnel postmeta.<br /><br />";
-			$info .= "wp_args: <pre>".print_r($wp_args, true)."</pre>";
+			//$info .= "wp_args: <pre>".print_r($wp_args, true)."</pre>";
 			//$info .= "Last SQL-Query: <pre>".$result->request."</pre>";
 			if ( $ids ) { $info .= "ids: ".$ids."<br />"; }
 			$info .= "field_check: ".$field_check."<br />";
@@ -2659,7 +2659,7 @@ function event_program_cleanup( $atts = [] ) {
 		if ( empty($posts) ) {
 		
 			$info .= "No matching posts found in initial quick query for program_items.<br />";
-			$info .= "wp_args: <pre>".print_r($wp_args, true)."</pre>";
+			//$info .= "wp_args: <pre>".print_r($wp_args, true)."</pre>";
 			//$info .= "Last SQL-Query: <pre>".$result->request."</pre>";
 			
 			// STILL no posts? Try a more expensive query...
