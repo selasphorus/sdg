eee8ec775cccf7baf5722cc1b8e44fa7a8acb7c8
atc <birdhive@gmail.com>
2023-06-27 21:36:56 -0400

    cont.

--- modules/events.php
+++ modules/events.php
@@ -2895,7 +2895,7 @@ function event_program_cleanup( $atts = [] ) {
 			foreach ( $posts AS $post_id ) {
 				
 				// Init
-				$info .= '<div style="border-bottom: 2px dashed purple; padding-bottom: 1.5rem; margin-bottom: 1rem;">';
+				$info .= '<div style="border-bottom: 2px solid purple; padding: 1rem; margin-bottom: 1rem;">';
 				$post_info = "";
 				$post_errors = false;
 				
@@ -2911,11 +2911,13 @@ function event_program_cleanup( $atts = [] ) {
     
 				if ( count($rows) > 0 ) {
 					$i = 0;
-					foreach ( $rows as $row ) {						
+					foreach ( $rows as $row ) {
+						$post_info .= '<div class="program_row" style="border: 1px solid green; padding: 1rem; font-size: 0.9rem;">';				
 						$arr_row_info = event_program_row_cleanup ( $post_id, $i, $row, "program_items" );								
 						$post_info .= $arr_row_info['info'];
 						$row_errors = $arr_row_info['errors'];
-						if ( $row_errors ) { $post_errors = true; }						
+						if ( $row_errors ) { $post_errors = true; }	
+						$post_info .= '</div>';					
 						$i++;				
 					}
 				} else {
