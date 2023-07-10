ab526b5dd944daa9149a103c1abe30cd089bd1d2
atc <birdhive@gmail.com>
2023-07-10 16:01:45 -0400

    cont.

--- inc/common_functions.php
+++ inc/common_functions.php
@@ -400,9 +400,9 @@ function sdg_post_thumbnail ( $args = array() ) {
         	// TODO: streamline this
         	if ( $img_id && is_array($image_gallery) && count($image_gallery) > 0 && $return == "html" ) {
         		$ts_info .= "image_gallery image<br />";
-        		$img_html .= '<div class="'.$classes.'">';
+        		//$img_html .= '<div class="'.$classes.'">';
         		$img_html = wp_get_attachment_image( $img_id, $img_size, false, array( "class" => "featured_attachment" ) );
-        		$img_html .= '</div><!-- .post-thumbnail -->';
+        		//$img_html .= '</div><!-- .post-thumbnail -->';
         	}
         	
         }
