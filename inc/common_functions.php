75fdbe5b57e1fc080045c34132ff578d6b288e94
atc <birdhive@gmail.com>
2023-07-10 15:04:44 -0400

    Corrected syntax

--- inc/common_functions.php
+++ inc/common_functions.php
@@ -357,7 +357,7 @@ function sdg_post_thumbnail ( $args = array() ) {
     $classes = "post-thumbnail sdg";
     if ( is_dev_site() ) { $classes .= " zoom-fade"; }
     if ( is_singular('event') ) { $classes .= " event-image"; }
-    if ( is_archive || is_post_type_archive() ) { $classes .= " float-left"; }
+    if ( is_archive() || is_post_type_archive() ) { $classes .= " float-left"; }
     
     // Retrieve the caption (if any) and return it for display
     if ( get_post( $img_id  ) ) {
