6297b4ccdb7fcbffd69368887adc085983724f45
atc <birdhive@gmail.com>
2023-07-19 18:00:38 -0400

    cont.

--- modules/groups.php
+++ modules/groups.php
@@ -69,7 +69,42 @@ function display_group_personnel ( $args = array() ) {
 				//$info .= "group_person: <pre>".print_r($group_person, true)."</pre>";
 				$title_id = $group_person['title'];
 				$title_term = get_term($title_id);
-				if ( $title_term ) { $info .= $title_term->name."<br />"; }
+				if ( $title_term ) { 
+				
+					$info .= $title_term->name."<br />";
+					
+					// Get all persons matching this group_id and title_id which are current
+					//...
+					// best to do this via a bidirectional field along the lines of repertoire_events rather than trying to query ACF repeater rows?
+					//...
+					
+					$wp_args = array(
+						'post_type'   => 'person',
+						'post_status' => 'publish',
+						//'posts_per_page' => 1,
+						'meta_query' => array(
+							'relation' => 'AND',
+							array(
+								'key'		=> "titles_XYZ_group", // name of custom field, with XYZ as a wildcard placeholder (must do this to avoid hashing)
+								'compare' 	=> 'LIKE',
+								'value' 	=> '"' . $group_id . '"', // matches exactly "123", not just 123. This prevents a match for "1234"
+							),
+							array(
+								'key'		=> "titles_XYZ_title", // name of custom field, with XYZ as a wildcard placeholder (must do this to avoid hashing)
+								'compare' 	=> 'LIKE',
+								'value' 	=> '"' . $title_id . '"', // matches exactly "123", not just 123. This prevents a match for "1234"
+							),
+						),
+						'fields' => 'ids',
+					);
+	
+					$query = new WP_Query( $wp_args );
+					$persons = $query->posts;
+					
+					foreach ( $persons as $person_id ) {
+						$info .= get_the_title($person_id)."<br />";
+					}
+				}
 			}
 		}
     	
