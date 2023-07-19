8b725299c6ac870e2f495d36f7259d3d7d1f18b7
atc <birdhive@gmail.com>
2023-07-19 18:01:55 -0400

    cont.

--- modules/groups.php
+++ modules/groups.php
@@ -101,6 +101,9 @@ function display_group_personnel ( $args = array() ) {
 					$query = new WP_Query( $wp_args );
 					$persons = $query->posts;
 					
+					$info .= "wp_args: <pre>".print_r($wp_args, true)."</pre>";
+					$info .= "persons: <pre>".print_r($persons, true)."</pre>";
+					
 					foreach ( $persons as $person_id ) {
 						$info .= get_the_title($person_id)."<br />";
 					}
