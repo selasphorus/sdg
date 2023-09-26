920f0b726e0afb7803ffc528df164e04734d889f
atc <birdhive@gmail.com>
2023-09-26 11:47:59 -0400

    Attempting to eliminate undefined index error

--- inc/admin_functions.php
+++ inc/admin_functions.php
@@ -2506,7 +2506,7 @@ function sdg_include_slug_in_search( $search, $query ) {
 	if ( $query->is_search() && $query->is_admin && 'event' == $query->query_vars['post_type'] ) {
 		$search = ''; // We will rebuild the entire clause
 		$searchand = '';
-		if ( isset($query->query_vars['search_terms']) ) {
+		if ( isset($query->query_vars) && isset($query->query_vars['search_terms']) ) {
 			foreach ( $query->query_vars['search_terms'] as $term ) {
 				$like = '%' . $wpdb->esc_like( $term ) . '%';
 				$search .= $wpdb->prepare( "{$searchand}(($wpdb->posts.post_title LIKE %s) OR ($wpdb->posts.post_content LIKE %s) OR ($wpdb->posts.post_name LIKE %s))", $like, $like, $like );
