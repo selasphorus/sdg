450aa531f608756456542e8e6bc6e0e8008d3c0e
atc <birdhive@gmail.com>
2023-09-27 18:46:43 -0400

    cont.

--- modules/webcasts.php
+++ modules/webcasts.php
@@ -355,7 +355,7 @@ function get_media_player ( $post_id = null, $status_only = false, $url = null )
             if ( $status_only == false ) {
                 $src = 'https://www.youtube.com/watch?v='.$video_id;
                 $youtube_ts = get_field('youtube_ts');
-                if ( $youtube_ts ) { $src .= "&t=".$youtube_ts; }
+                if ( $youtube_ts ) { $src .= "&start=".$youtube_ts; }
                 //$player = do_shortcode('[video src="'.$src.'"]'); //height="300"
                 $player .= '<div class="responsive-youtube"><iframe width="850" height="475" src="https://www.youtube.com/embed/'.$video_id.'?controls=1" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
             }
