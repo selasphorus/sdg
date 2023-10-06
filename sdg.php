a1f5a2159e3cb8646a029ea5f776b7df93b695f5
atc <birdhive@gmail.com>
2023-10-06 15:30:21 -0400

    cont.

--- sdg.php
+++ sdg.php
@@ -2386,11 +2386,14 @@ function widget_logic_tmp () {
 		$xml .= "&lt;index&gt;".$key."&lt;/index&gt;<br />";
 		foreach ( $arr_option as $option => $value ) {
 			$xml .= "&lt;".$option."&gt;";
-			if ( count($value) == 1 ) {
+			foreach ( $value as $k => $v ) {
+				$xml .= "&lt;".$k."&gt;".$v."&lt;/".$k"&gt;<br />";
+			}
+			/*if ( count($value) == 1 ) {
 				$xml .= $value[0];
 			} else {
 				$xml .= print_r($value,true); //$xml .= $value;
-			}			
+			}*/		
 			$xml .= "&lt;/".$option."&gt;<br />";
 		}
 		//$xml .= print_r($option,true);
