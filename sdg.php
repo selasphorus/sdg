58986d747c54193043b1e00a706e4dae4e422914
atc <birdhive@gmail.com>
2023-10-10 16:32:35 -0400

    Cont. work on widget logic

--- sdg.php
+++ sdg.php
@@ -2384,7 +2384,7 @@ function widget_logic_tmp () {
 	$i = 0;
 	$logic_options = get_option('widget_logic_options');
 	foreach ( $logic_options as $widget => $conditions ) {
-		$info .= "<pre>widget: ".$widget." ==> ".print_r($conditions,true)."</pre><hr /><hr />"; // tft
+		//$info .= "<pre>widget: ".$widget." ==> ".print_r($conditions,true)."</pre><hr /><hr />"; // tft
 		$xml .= "&lt;option&gt;<br />";
 		$xml .= "&#9;"."<strong>"."&lt;widget&gt;".$widget."&lt;/widget&gt;"."</strong><br />";
 		//$xml .= "&lt;index&gt;".$key."&lt;/index&gt;<br />";
@@ -2418,6 +2418,9 @@ function widget_logic_tmp () {
 		if ( $i >= 1 ) { break; } // tft
 	} // end foreach
 	$xml .= "&lt;/options&gt;<br />";
+	//
+	$info .= $xml;
+	//
 	//$info .= "<pre>".print_r($xml,true)."<pre>";	
 	return $info;	
 }
