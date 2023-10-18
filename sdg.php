5fade7900c1c6cc83fe07a69b20aeb470443c5db
atc <birdhive@gmail.com>
2023-10-18 17:20:33 -0400

    cont. -- divider char based on option format, not csv tests

--- sdg.php
+++ sdg.php
@@ -2670,9 +2670,9 @@ function update_snippet_logic ( $snippet_id = null ) {
 			$info .= "key: $key => ".print_r($$key, true)."<br />"; // ." [count: ".count($$key)."]"
 			if ( $key == 'target_by_url' || $key == 'exclude_by_url' ) {
 				// Legacy fields => ignore or translate
-				$urls = explode(" | ",$$key);
+				$urls = explode("/ /",$$key); //$urls = explode(" | ",$$key);
 				//
-				$$key = str_replace(" | ","\n",$$key);
+				$$key = str_replace("/ /","/\n/",$$key);
 				update_field( $key, $$key, $snippet_id );
 				//
 				if ( is_array($urls)) {
