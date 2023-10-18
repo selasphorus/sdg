7ca6f9e874553847c1c9f94a854db36770f2838e
atc <birdhive@gmail.com>
2023-10-18 19:58:53 -0400

    cont.

--- sdg.php
+++ sdg.php
@@ -2962,15 +2962,17 @@ function convert_widgets_to_snippets ( $atts = [] ) {
 					$existing = get_field( 'target_by_url' );
 					if ( ! is_array($existing) ) { $existing = array(); }
 					$additions = $subconditions['urls'];
+					$info .= "existing: ".print_r($existing, true)."<br />";
+					$info .= "additions: ".print_r($additions, true)."<br />";
 					//$str_additions = $subconditions['urls'];
 					//$str_additions = preg_replace("/[\r\n]+/", "\n", $str_additions);
 					//$additions = explode("\n",$str_additions);
-					if ( !empty($additions) ) {
+					/*if ( !empty($additions) ) {
 						$updated = array_unique(array_merge($existing, $additions));
 						if ( update_field( 'target_by_url', $updated ) ) {
 							$info .= "updated repeater field: target_by_url<br />";
 						}
-					}
+					}*/
 									
 				}		
 					
@@ -2985,15 +2987,17 @@ function convert_widgets_to_snippets ( $atts = [] ) {
 					$existing = get_field( 'exclude_by_url' );
 					if ( ! is_array($existing) ) { $existing = array(); }
 					$additions = $subconditions['urls_invert'];
+					$info .= "existing: ".print_r($existing, true)."<br />";
+					$info .= "additions: ".print_r($additions, true)."<br />";
 					//$str_additions = $subconditions['urls_invert'];
 					//$str_additions = preg_replace("/[\r\n]+/", "\n", $str_additions);
 					//$additions = explode("\n",$str_additions);
-					if ( !empty($additions) ) {
+					/*if ( !empty($additions) ) {
 						$updated = array_unique(array_merge($existing, $additions));
 						if ( update_field( 'exclude_by_url', $updated ) ) {
 							$info .= "updated repeater field: exclude_by_url<br />";
 						}
-					}
+					}*/
 					
 				}
 				
