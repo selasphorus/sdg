326dd7b16c0c9c4c47f2d27c6cfee6aa7c7a9ea1
atc <birdhive@gmail.com>
2023-10-20 11:09:40 -0400

    cont.

--- sdg.php
+++ sdg.php
@@ -2641,7 +2641,7 @@ function show_snippets ( $atts = [] ) {
 function update_snippet_logic ( $snippet_id = null ) {
 
 	// TS/logging setup
-    $do_ts = false; 
+    $do_ts = true; 
     $do_log = false;
     sdg_log( "divline2", $do_log );
     
