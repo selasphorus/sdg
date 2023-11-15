fc59180b3af44bc79234c98a8182c8f728e110ab
atc <birdhive@gmail.com>
2023-11-14 21:51:27 -0500

    cont.

--- sdg.php
+++ sdg.php
@@ -3651,13 +3651,13 @@ function update_snippet_logic ( $atts = [] ) { //function update_snippet_logic (
 							$condition_info .= "condition: <pre>".print_r($condition, true)."</pre>";
 							continue;
 						}					
-					} else if ( gettype($condition) == "string" ) {
+					} /*else if ( gettype($condition) == "string" ) {
 						$p_id = intval($condition);
 						$condition_info .= "p_id: ".$p_id."<br />";
 						// Check to see if p_id is a valid post id
 						$post = get_post( $p_id );
 						if ( $post ) { $matched_post_id = $p_id; }
-					} else {
+					} */else {
 						$condition_info .= "condition: ".$condition." [".gettype($condition)."]<br />";
 					}					
 					//
