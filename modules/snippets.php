817ae461ca99b8eb3a2bf1b66e49933405a74bec
atc <birdhive@gmail.com>
2023-11-17 14:20:22 -0500

    cont.

--- modules/snippets.php
+++ modules/snippets.php
@@ -1326,7 +1326,7 @@ function delete_widgets ( $atts = [] ) {
 				$info .= "&rarr; Do NOT delete this widget!<br />";
 			}		
 			
-			if ( $i > $limit ) { break; }
+			if ( $i > $limit && $limit > 0 ) { break; }
 		}
 		//
 		//$info .= "REVISED widgets: <pre>".print_r($widgets,true)."</pre><hr /><hr />";
