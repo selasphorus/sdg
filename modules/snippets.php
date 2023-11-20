1fbefeb6235041372d7c3d4ea578065a7b6db841
atc <birdhive@gmail.com>
2023-11-20 13:24:18 -0500

    cont.

--- modules/snippets.php
+++ modules/snippets.php
@@ -1447,8 +1447,8 @@ function convert_post_widgets_to_snippets ( $atts = [] ) {
 					$info .= "update FAILED for field: ".$update_key." for snippet_id: $snippet_id<br />";
 				}
 			} else {
-				$info .= "field '$update_key'<br />";
-				$info .= "=> <pre>".print_r($updated_field_value, true)."</pre>";
+				//$info .= "field '$update_key'<br />";
+				//$info .= "=> <pre>".print_r($updated_field_value, true)."</pre>";
 			}
 			//$info .= "&rarr;&rarr; update_snippet_logic<br />";
 			//$info .= update_snippet_logic ( array( 'snippet_id' => $snippet_id ) ); //$info .= '<div class="code">'.update_snippet_logic ( $snippet_id ).'</div>';
