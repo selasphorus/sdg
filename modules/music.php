d1ccac908a21bf1825b91fb899ef68cc3cecfa30
atc <birdhive@gmail.com>
2023-11-30 11:06:49 -0500

    cont.

--- modules/music.php
+++ modules/music.php
@@ -2472,7 +2472,7 @@ function format_search_results ( $post_ids, $search_type = "choirplanner" ) {
     
     //$ts_info .= 'rep_ids: <pre>'.print_r($rep_ids, true).'</pre>';
     
-    /////$rep_ids = array_unique($rep_ids);
+    $rep_ids = array_unique($rep_ids);
     //$info .= 'array_unique rep_ids: <pre>'.print_r($rep_ids, true).'</pre>';
     //$info .= "<br />+++++++++++<br />";
     
@@ -2494,9 +2494,7 @@ function format_search_results ( $post_ids, $search_type = "choirplanner" ) {
     $i = 0;
     foreach ( $rep_ids as $rep_id ) {
         
-        $ts_info .= 'rep_id: <pre>'.print_r($rep_id, true).'</pre>';
-        continue;
-        //if ( !is_int($rep_id) ) { $info .= "rep_id $rep_id NOT IS INT!<br />"; continue; }
+        //$ts_info .= 'rep_id: <pre>'.print_r($rep_id, true).'</pre>';
         
         $post_id = $rep_id;
         $post_title = get_the_title($post_id);
