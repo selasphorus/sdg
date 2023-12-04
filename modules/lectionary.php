cbf2b886e98ff18c226d9a35472f815070376793
atc <birdhive@gmail.com>
2023-12-04 13:07:33 -0500

    cont.

--- modules/lectionary.php
+++ modules/lectionary.php
@@ -841,6 +841,7 @@ function calc_date_from_str( $year = null, $date_calculation_str = null, $verbos
 	} else if ( count($calc_bases) == 1 ) {
 		$calc_basis = $calc_bases[0][0];
 		$calc_basis_field = $calc_bases[0][1];
+		$info .= "calc_bases: <pre>".print_r($calc_bases, true)."</pre>";
 		if ( $verbose == "true" ) { $info .= "liturgical calc_basis: $calc_basis // $calc_basis_field<br />"; } // $info .= "calc_basis_field: $calc_basis_field -- "; // tft            
 	}
 	
