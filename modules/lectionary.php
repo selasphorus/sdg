0975553253912fe31780095ac714de4f1702555e
atc <birdhive@gmail.com>
2023-12-04 13:15:11 -0500

    cont.

--- modules/lectionary.php
+++ modules/lectionary.php
@@ -839,9 +839,11 @@ function calc_date_from_str( $year = null, $date_calculation_str = null, $verbos
 		$calc['calc_info'] = $info;
 		return $calc; // abort early -- we don't know what to do with this date_calculation_str
 	} else if ( count($calc_bases) == 1 ) {
+		$cb = $calc_bases[0];
 		$calc_basis = array_key_first($calc_bases[0]);
 		$calc_basis_field = $calc_bases[$calc_basis];
 		$info .= "calc_bases: <pre>".print_r($calc_bases, true)."</pre>";
+		$info .= "cb: <pre>".print_r($cb, true)."</pre>";
 		if ( $verbose == "true" ) { $info .= "liturgical calc_basis: $calc_basis // $calc_basis_field<br />"; } // $info .= "calc_basis_field: $calc_basis_field -- "; // tft            
 	}
 	
