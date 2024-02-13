d60547abc144a51cbb90d296cf891e722a9ce280
atc <birdhive@gmail.com>
2024-02-13 18:14:32 -0500

    cont.

--- inc/common_functions.php
+++ inc/common_functions.php
@@ -719,7 +719,7 @@ function get_related_posts( $post_id = null, $related_post_type = null, $related
         	//$info .= "related_posts: ".print_r($related_posts,true);
         	$info .= "related_posts->posts:<pre>".print_r($related_posts->posts,true)."</pre>";
         	$info .= "wp_args:<pre>".print_r($wp_args,true)."</pre>";
-            //$info = $related_posts->posts();
+            $info = $related_posts->posts;
         }
         
         /*
