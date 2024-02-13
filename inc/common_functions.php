14164a7d547632485aafd1cf5fe763b452b4cfd5
atc <birdhive@gmail.com>
2024-02-13 18:12:34 -0500

    cont. ts -- changed call from data to posts for get_related_posts fcn

--- inc/common_functions.php
+++ inc/common_functions.php
@@ -715,13 +715,15 @@ function get_related_posts( $post_id = null, $related_post_type = null, $related
             }
             $info = $related_id;
         } else {
-            $info = $related_posts->data();
+            $info = $related_posts->posts();
         }
         
+        /*
         $info .= "<br />";
         //$info .= "related_posts: ".print_r($related_posts,true);
         $info .= "related_posts->posts:<pre>".print_r($related_posts->posts,true)."</pre>";
         $info .= "wp_args:<pre>".print_r($wp_args,true)."</pre>";
+        */
         
     } else {
     	$info = "No matching posts found for wp_args: ".print_r($wp_args,true);
