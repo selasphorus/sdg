8bbc91fc1c0204bb6dddd4e3f2224432f2e7cbb6
atc <birdhive@gmail.com>
2024-02-20 13:25:13 -0500

    cont.

--- inc/common_functions.php
+++ inc/common_functions.php
@@ -1603,7 +1603,7 @@ function display_all_postmeta( $args = array() ) {
     $info .= "<h3>Post Meta Data for post with ID $post_id</h3>";
     $info .= "<pre>";
     foreach ( $postmeta as $key => $value ) {
-    	if ( strpos("_",$key) !== 0 ) {
+    	if ( strpos($key,"_") !== false ) {
     		$info .= $key." => ".print_r($value,true);
     	}
     }
