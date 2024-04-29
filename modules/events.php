05cc5b41e2757128e4bfad6b547944f9e8b672ac
atc <birdhive@gmail.com>
2024-04-29 15:57:29 -0400

    cont.

--- modules/events.php
+++ modules/events.php
@@ -1156,9 +1156,9 @@ function get_event_program_items( $atts = [] ) {
 			// Insert row_info for troubleshooting
 			if ( devmode_active() ) {
 				if ( $display == 'table' ) {
-					$table .= '<div class="troubleshooting">'.$row_info.'</div>'; //$row_info; // Display comments w/ in row for ease of parsing dev notes
+					$table .= '<div class="troubleshooting">row_info:<br />'.$row_info.'</div>'; //$row_info; // Display comments w/ in row for ease of parsing dev notes
 				} else {
-					$ts_info .= $row_info; //$info .= '<div class="troubleshooting">'.$row_info.'</div>';
+					$ts_info .= 'row_info:<br />'.$row_info; //$info .= '<div class="troubleshooting">'.$row_info.'</div>';
 				}
 			}
 			
