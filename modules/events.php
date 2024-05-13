423e763e570f99974ce224c524947f79e08cc6ce
atc <birdhive@gmail.com>
2024-05-13 16:01:14 -0400

    display choral rep

--- modules/events.php
+++ modules/events.php
@@ -293,7 +293,7 @@ function get_music_department_info( $post_id = null ) {
     $info .= "<h3>Music Staff:</h3>";
     $staff = get_field( 'music_staff', $post_id );
     foreach ( $staff as $post ) {
-		if ( is_object($post)) { $info .= $post->post_title."<br />"; }
+    	$info .= $post->post_title."<br />";
 	}
     
     // Groups
@@ -321,6 +321,9 @@ function get_music_department_info( $post_id = null ) {
     // Repertoire
     $info .= "<h2>Repertoire</h2>";
     $choral_rep = get_field( 'choral_rep', $post_id );
+    foreach ( $choral_rep as $post ) {
+    	$info .= $post->post_title."<br />";
+	}
     $opening_voluntary = get_field( 'opening_voluntary', $post_id );
     $closing_voluntary = get_field( 'closing_voluntary', $post_id );
     //
