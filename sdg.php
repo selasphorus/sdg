e55bb1d242fd54301357d0acabf4e29760d706c5
atc <birdhive@gmail.com>
2024-05-20 12:13:33 -0400

    Module options, not per cpt -- at least for now

--- sdg.php
+++ sdg.php
@@ -457,14 +457,17 @@ if ( function_exists('acf_add_options_page') ) {
     	
     	// Deal w/ modules whose names don't perfectly match their CPT names
     	if ( $module == "people" ) {
+    		$primary_cpt = "person";
     		$cpt_names[] = "person";
     	} else if ( $module == "music" ) {
+    		$primary_cpt = "repertoire";
     		$cpt_names[] = "repertoire";
     		//$cpt_names[] = "edition";
     		//$cpt_names[] = "publisher";
     		//$cpt_names[] = "publication";
     		//$cpt_names[] = "music_list";
     	} else if ( $module == "lectionary" ) {
+    		$primary_cpt = "lectionary";
     		//$cpt_names[] = "bible_book";
     		//$cpt_names[] = "reading";
     		//$cpt_names[] = "lectionary";
@@ -473,36 +476,44 @@ if ( function_exists('acf_add_options_page') ) {
     		//$cpt_names[] = "collect";
     		//$cpt_names[] = "psalms_of_the_day";
     	} else if ( $module == "sermons" ) {
+    		$primary_cpt = "sermon";
     		$cpt_names[] = "sermon";
     		$cpt_names[] = "sermon_series";
     	} else if ( $module == "events" ) {
+    		$primary_cpt = "event";
     		$cpt_names[] = "event";
     		$cpt_names[] = "event_series";
     	} else if ( $module == "organs" ) {
+    		$primary_cpt = "organ";
     		$cpt_names[] = "organ";
     		//$cpt_names[] = "builder"; // division, manual, stop
-    	} else if ( $module == "venues" ) {
-    		$cpt_names[] = "venue";
-    		//$cpt_names[] = "address";
     	} else {
     		$cpt_name = $module;
     		// Make it singular -- remove trailing "s"
     		if ( substr($cpt_name, -1) == "s" && $cpt_name != "press" ) { $cpt_name = substr($cpt_name, 0, -1); }
+    		$primary_cpt = $cpt_name;
     		$cpt_names[] = $cpt_name;
     	}
     	
-    	// Add options pages per cpt
-		foreach ( $cpt_names as $cpt ) {
+    	// Add module options page
+    	acf_add_options_sub_page(array(
+			'page_title'	=> ucfirst($module).' Module Options',
+			'menu_title'    => ucfirst($module).' Module Options',//'menu_title'    => 'Archive Options', //ucfirst($cpt_name).
+			'menu_slug' 	=> $module.'-module-options',
+			'parent_slug'   => 'edit.php?post_type='.$primary_cpt,
+		));
+			
+    	// Add options pages per cpt?
+		/*foreach ( $cpt_names as $cpt ) {
 		
 			acf_add_options_sub_page(array(
 				'page_title'     => ucfirst($cpt).' CPT Options',
-				'menu_title'    => 'Archive Options', //ucfirst($cpt_name).
+				'menu_title'    => ucfirst($cpt_name).' Options',//'menu_title'    => 'Archive Options', //ucfirst($cpt_name).
 				'menu_slug' 	=> $cpt.'-cpt-options',
 				'parent_slug'    => 'edit.php?post_type='.$cpt,
 			));
 		
-		}
-		
+		}*/		
 		
 	}
 
