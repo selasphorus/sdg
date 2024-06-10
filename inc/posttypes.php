77f9a1ed22bea1959fff508db1fe2f57389a35e3
atc <birdhive@gmail.com>
2024-06-10 14:07:17 -0400

    Fixed check for plugin active

--- inc/posttypes.php
+++ inc/posttypes.php
@@ -209,7 +209,7 @@ if ( in_array('snippets', $sdg_modules ) ) {
 // WIP -- transition to separate plugins including WHx4
 //if ( in_array('people', $sdg_modules ) && !post_type_exists('person') ) {
 if ( in_array('people', $sdg_modules ) 
-&& in_array( trailingslashit( WP_PLUGIN_DIR ) . 'whx4/whx4.php', wp_get_active_and_valid_plugins() )
+&& !in_array( trailingslashit( WP_PLUGIN_DIR ) . 'whx4/whx4.php', wp_get_active_and_valid_plugins() )
 //&& !is_plugin_active( WP_PLUGIN_DIR . '/whx4/whx4.php' ) 
 ) {
 	// Person
