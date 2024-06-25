362370d92e2f21601f5df448127a68dd309bfa32
atc <birdhive@gmail.com>
2024-06-25 13:41:53 -0400

    cont.

--- modules/venues.php
+++ modules/venues.php
@@ -82,24 +82,29 @@ function get_cpt_venue_content( $post_id = null ) {
     //$info .= display_postmeta( array('post_id' => $post_id) );
     
     $venue_filename = get_post_meta( $post_id, 'venue_filename', true );
-    $info .= "<strong>venue_filename</strong>: ".print_r($venue_filename, true)."<br />";
+    $info .= '<strong>venue_filename</strong>: <div class="xxx wip">'.print_r($venue_filename, true)."</div>";
     
     if ( !queenbee() ) {
     	// If not queenbee, show content instead of acf_form
     	// WIP
     	//$settings = array( 'fields' => array( 'venue_info_ip', 'venue_info_vp', 'venue_sources', 'venue_html_ip', 'organs_html_ip', 'organs_html_vp' ) );
     	$venue_info_ip = get_post_meta( $post_id, 'venue_info_ip', true );
-    	$info .= "<strong>venue_info_ip</strong>: ".$venue_info_ip."<br />";
+    	$info .= '<strong>venue_info_ip</strong>: <div class="xxx wip">'.$venue_info_ip."</div>";
+    	//<div class="source venue_source wip">
     	$venue_info_vp = get_post_meta( $post_id, 'venue_info_vp', true );
-    	$info .= "<strong>venue_info_vp</strong>: ".$venue_info_vp."<br />";
+    	$info .= '<strong>venue_info_vp</strong>: <div class="xxx wip">'.$venue_info_vp."</div>";
+    	
     	$venue_sources = get_post_meta( $post_id, 'venue_sources', true );
-    	$info .= "<strong>venue_sources</strong>: ".$venue_sources."<br />";
+    	$info .= '<strong>venue_sources</strong>: <div class="xxx wip">'.$venue_sources."</div>";
+    	
     	$venue_html_ip = get_post_meta( $post_id, 'venue_html_ip', true );
-    	$info .= "<strong>venue_html_ip</strong>: ".$venue_html_ip."<br />";
+    	$info .= '<strong>venue_html_ip</strong>: <div class="xxx wip">'.$venue_html_ip."</div>";
+    	
     	$organs_html_ip = get_post_meta( $post_id, 'organs_html_ip', true );
-    	$info .= "<strong>organs_html_ip</strong>: ".$organs_html_ip."<br />";
+    	$info .= '<strong>organs_html_ip</strong>: <div class="xxx wip">'.$organs_html_ip."</div>";
+    	
     	$organs_html_vp = get_post_meta( $post_id, 'organs_html_vp', true );
-    	$info .= "<strong>organs_html_vp</strong>: ".$organs_html_vp."<br />";
+    	$info .= '<strong>organs_html_vp</strong>: <div class="xxx wip">'.$organs_html_vp."</div>";
     }
     
     return $info;
