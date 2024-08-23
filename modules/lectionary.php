f5549cca85cc3e295e352b7f13e32fc9e91756b8
atc <birdhive@gmail.com>
2024-08-23 13:11:00 -0400

    cont.

--- modules/lectionary.php
+++ modules/lectionary.php
@@ -1344,8 +1344,9 @@ function calc_date_from_components ( $args = array() ) {
 		$calc_date = $basis_date;
 		$info .= "date to be calculated is same as basis_date.<br />";
 		
-	//} else if ( $calc_basis is a date... ) {
+	} else if ( is_numeric($basis_date) ) {
 	
+		$info .= "basis_date is a timestamp.<br />";
 	
 	} else {
         
