319b1c4c82c9fefcc88e3920fa87b5b5c94ad62a
atc <birdhive@gmail.com>
2024-09-11 12:15:24 -0400

    New 'extract_numbers' function

--- includes/admin_functions.php
+++ includes/admin_functions.php
@@ -65,6 +65,14 @@ function contains_numbers ( $string ) {
 	
 }
 
+function extract_numbers ( $string ) {
+
+	preg_match_all('/first|second|third|fourth|fifth|sixth|seventh|eighth|ninth|tenth|one|two|three|four|five|six|seven|eight|nine|ten[0-9]+/', $string, $matches, PREG_OFFSET_CAPTURE)
+	
+	return $matches;
+	
+} 
+
 /*** POST TITLE/SLUG FUNCTIONS ***/
 
 /*function remove_accents ( $str ) {
