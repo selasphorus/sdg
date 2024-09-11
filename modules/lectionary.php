5b7966c120ea4c6c2427e751143ec6856b954c0b
atc <birdhive@gmail.com>
2024-09-11 12:26:23 -0400

    cont.

--- modules/lectionary.php
+++ modules/lectionary.php
@@ -1488,12 +1488,12 @@ function calc_date_from_components ( $args = array() ) {
 				$numbers = extract_numbers($date_calc_str);
 				if ( $verbose == "true" ) { $info .= "numbers: ".print_r($numbers,true)."<br />"; }
 				if ( count($numbers) == 1 ) {
-					$calc_interval = $numbers[0];
+					//$calc_interval = $numbers[0];
 				} else {
 					$calc_interval = str_replace([$calc_basis, $calc_weekday, $calc_boia], '', strtolower($date_calc_str) );
 					$calc_interval = str_replace(['the', 'th', 'nd', 'rd', 'st'], '', strtolower($date_calc_str) );
 				}
-				//$calc_interval = trim( $calc_interval );
+				$calc_interval = trim( $calc_interval );
 			}
 			if ( $verbose == "true" && !empty($calc_interval) ) { $info .= "calc_interval: $calc_interval<br />"; }
 			
