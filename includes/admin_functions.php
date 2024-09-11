ac183927f8ee0aa7468a3b1ba5176fc3e9f4f45c
atc <birdhive@gmail.com>
2024-09-11 13:02:01 -0400

    cont.

--- includes/admin_functions.php
+++ includes/admin_functions.php
@@ -41,15 +41,25 @@ function word_to_digit ( $word ) {
     $words_to_digits = [
         'zero' => 0,
         'one' => 1,
+        'first' => 1,
         'two' => 2,
+        'second' => 2,
         'three' => 3,
+        'third' => 3,
         'four' => 4,
+        'fourth' => 4,
         'five' => 5,
+        'fifth' => 5,
         'six' => 6,
+        'sixth' => 6,
         'seven' => 7,
+        'seventh' => 7,
         'eight' => 8,
+        'eighth' => 8,
         'nine' => 9,
+        'ninth' => 9,
         'ten' => 10,
+        'tenth' => 10,
     ];
 
     return isset($words_to_digits[$word]) ? $words_to_digits[$word] : null;
