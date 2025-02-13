f304de61b05dfc4fb960e130484e1cc0be6d1df3
atc <birdhive@gmail.com>
2025-02-13 17:41:02 -0500

    Enable admin_tag taxonomy for CPT project

--- includes/taxonomies.php
+++ includes/taxonomies.php
@@ -79,7 +79,7 @@ function register_taxonomy_admin_tag() {
 			'assign_terms'  =>   'assign_'.$cap.'_terms',
 		);
 	}*/
-    register_taxonomy( 'admin_tag', [ 'admin_note', 'attachment', 'bible_book', 'collect', 'collection', 'data_table', 'edition', 'ensemble', 'event', 'event-recurring', 'event_series', 'lectionary', 'liturgical_date', 'liturgical_date_calc', 'location', 'music_list', 'page', 'person', 'post', 'product', 'psalms_of_the_day', 'publication', 'publisher', 'reading', 'repertoire', 'sermon', 'sermon_series', 'snippet', 'venue' ], $args );
+    register_taxonomy( 'admin_tag', [ 'admin_note', 'attachment', 'bible_book', 'collect', 'collection', 'data_table', 'edition', 'ensemble', 'event', 'event-recurring', 'event_series', 'lectionary', 'liturgical_date', 'liturgical_date_calc', 'location', 'music_list', 'page', 'person', 'post', 'product', 'project', 'psalms_of_the_day', 'publication', 'publisher', 'reading', 'repertoire', 'sermon', 'sermon_series', 'snippet', 'venue' ], $args );
 }
 add_action( 'init', 'register_taxonomy_admin_tag' );
 
