f160ceee629902cbffea6c5e72c4379204a9e3d3
atc <birdhive@gmail.com>
2025-02-20 11:57:21 -0500

    cont.

--- includes/common_functions.php
+++ includes/common_functions.php
@@ -109,7 +109,7 @@ function sdg_post_title ( $args = array() ) {
 		$replace   = array('<span class="emtitle">', '</span>');
 		$title = str_replace($find, $replace, $title);
     	//
-		$find = array('*', '*');
+		$find = array('{', '}');
 		$replace   = array('<span class="emtitle">', '</span>');
 		$title = str_replace($find, $replace, $title);
 		//
