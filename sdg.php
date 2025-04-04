b11b2bb2e2521a5755a893d431e31fca4f82da77
atc <birdhive@gmail.com>
2025-04-04 14:39:20 -0400

    cont.

--- sdg.php
+++ sdg.php
@@ -37,7 +37,7 @@ $plugins_dir = WP_CONTENT_DIR . '/plugins/';
 
 // This is a temporary solution for making classes from WHx4 available to sdg_acf_admin_footer
 // TODO: figure out best approach to dependencies -- probably shared autloader?
-$required_file = $plugins_dir . 'whx4/vendor/autoload.php';
+$required_file = plugins_url().'whx4/vendor/autoload.php';
 //if ( is_plugin_active('whx4/whx4.php') ) {
 if ( file_exists( $required_file ) ) {
 	require_once( $required_file );
@@ -1716,7 +1716,7 @@ function sdg_custom_post_content() {
 		$info .= "I got this new ".$post_type.": <pre>".print_r($p,true)."</pre>";
 	} else {
 		$info .= "Class $post_type does not exist! :-(<br />";
-		$info .= "Maybe this required_file couldn't be found? :".WP_CONTENT_DIR . '/plugins/'. 'whx4/vendor/autoload.php'."<br />";
+		$info .= "Maybe this required_file couldn't be found? :".plugins_url(). 'whx4/vendor/autoload.php'."<br />";
 	}
 	$cpt_function = "get_cpt_".$post_type."_content";
 	if ( function_exists($cpt_function) ) {
