3c8b5549d42eb2e2436016da88f2375c472ab655
atc-mba25 <birdhive@gmail.com>
2025-10-09 16:44:23 -0400

    cont.

--- modules/lectionary.php
+++ modules/lectionary.php
@@ -618,7 +618,7 @@ function getLitDateData( array $args = [] ): array|string
             if ( $secondaryPost ) { $output .= $secondaryPost['post']->post_title; }
         } else if ( $args['return'] === 'formatted' ) {
             $output = "";
-            $output .= "debug: [$debug]<br />";
+            //$output .= "debug: [$debug]<br />";
             $data = formatLitDateData( $litdateData, $args );
             $output .= $data;
         }
@@ -649,7 +649,7 @@ function formatLitDateData( $litDateData = [], $args = [] )
     if ( $args[ 'admin' ] ) { $admin = $args[ 'admin' ]; } else { $admin = false; }
     if ( $args[ 'debug' ] ) { $debug = $args[ 'debug' ]; } else { $debug = false; }
     //
-    if ( $debug ) { $output .= "args: <pre>".print_r($args,true)."</pre>"; }
+    //if ( $debug ) { $output .= "args: <pre>".print_r($args,true)."</pre>"; }
 
     foreach ( $litDateData as $dateStr => $typeGroups ) {
         $output .= "<div class='liturgical-date-block'>";
