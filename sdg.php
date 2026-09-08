90bec9a787cb9a20fbf66da7a7b0e4536d23f402
atc-mba25 <birdhive@gmail.com>
2026-09-08 10:16:37 -0400

    vnu

--- sdg.php
+++ sdg.php
@@ -9,7 +9,7 @@
  * Description: Custom post types, taxonomies and functions for music and more
  * Dependencies:	  
  * Requires Plugins:  whx4-core, stc
- * Version: 1.260828
+ * Version: 1.260908
  * Author: atc
  * Author URI:
  * License:
